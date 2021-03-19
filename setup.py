9e537487ee6e13fc751fb8453c263b7ece97882a
Vivien Walter <vwalter@marks-mac-mini.home>
2021-03-19 11:42:11 +0000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+	name = "md2stl",
+	version = "0.1",
+	author = "Vivien WALTER",
+	author_email = "walter.vivien@gmail.com",
+	description = (
+	"Python tools to convert a MD simulation file into a .stl file ready to be 3d printed."
+	),
+	license = "GPL3.0",
+	url='https://github.com/vivien-walter/md2stl',
+	download_url = '',
+	packages=[
+	'md2stl'
+	],
+	install_requires=[
+	'numpy',
+	'numpy-stl',
+	'MDAnalysis',
+	'tqdm'
+	]
+)
